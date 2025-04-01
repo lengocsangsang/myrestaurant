c7775a580b363f013259119811009615993e42dc
lengocsangsang <lesang88@gmail.com>
2025-04-01 15:38:03 +0700

    initial

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5e24b7f0-e601-40d9-b9b3-deb92f7a6e0f';
+export const MODAL_CLOSE_SEC = 2.5;
